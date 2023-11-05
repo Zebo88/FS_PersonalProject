031282b33a94d7330dc0f7a4b4ef5788381e046c
Zach Fullmer <zachfullmer@Zachs-MacBook-Pro.local>
2023-11-04 22:26:35 -0600

    Added media queries to change text for phone size, added JS for return button, and cleaned up some text in paragraphs.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
-function onClick(){
-  alert("Form Submitted!");
+/* JS to alert the user that clicking on the Return button of the formsubmitted page will
+    return the user to the Home page, then it will return to the Home page. */
+function returnHome(){
+  alert("You are returning to the Home page!")
+  location.replace("./index.html");
 }
